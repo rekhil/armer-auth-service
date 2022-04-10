52455fa2a349cd2c25256dd57d938856ed35feb4
Rekhil Das <rekhildevadasan@gmail.com>
2022-04-10 13:40:01 +0530

    Initial Commit

---
+++ src/backend/armer-auth-service/armer-auth-service/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace armer_auth_service.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "API is running successfully";
+        }
+    }
+}
